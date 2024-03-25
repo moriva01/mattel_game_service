8c17f0b19fc97f6da9ebf273a51bbdd604c71aa2
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-25 10:19:05 -0500

    Changes to be committed: 	new file:   .vscode/settings.json 	modified:   controlador/operaciones.cpp 	modified:   vista/mattel_game_service.cpp 	modified:   vista/mattel_game_service.exe

--- controlador/operaciones.cpp
+++ controlador/operaciones.cpp
@@ -1,3 +1,132 @@
-//aqui se haran los metodos que unen la clase main con las clases del modelo
+// aqui se haran los metodos que unen la clase main con las clases del modelo
 
 #include "../modelo/ahorcado.cpp"
+
+vector<video_juego> lista_juegos;
+
+void ingresar_nuevos_juegos()
+{
+    video_juego aux;
+    string nombre;
+    int cant_jugadores;
+    int categoria;
+    string desarrollador;
+    int anio_lanzamiento;
+    int plataforma;
+
+    cout << "Digite el nombre del video juego: " << endl;
+    cin >> nombre;
+
+    for (video_juego p : lista_juegos) // recorre la lista
+    {
+
+        if (nombre == p.getNombre()) // mira si el documento ya esta registrado
+        {
+            cout << "el juego ya se encuentra registrado" << endl;
+            break;
+        }
+    }
+    aux.setNombre(nombre);
+    cout << "Digite la canditad de jugadores: " << endl;
+    cin >> cant_jugadores;
+    aux.setCantJugadores(cant_jugadores);
+    cout << "Digite la categoria del video juego: \n"
+         << "1- arcade \n 2- aventura \n 3- puzzle \n 4- estrategia \n 5- disparos" << endl;
+    cin >> categoria;
+
+    switch (categoria)
+    {
+    case 1:
+        aux.setCategoria("arcade");
+        break;
+    case 2:
+        aux.setCategoria("aventura");
+        break;
+    case 3:
+        aux.setCategoria("puzzle");
+        break;
+    case 4:
+        aux.setCategoria("estrategia");
+        break;
+    case 5:
+        aux.setCategoria("disparos");
+        break;
+
+    default:
+        cout << "opcion no valida" << endl;
+        break;
+    }
+
+    cout << "Digite el nombre del desarrollador del video juego: " << endl;
+    cin >> desarrollador;
+    aux.setDesarrollador(desarrollador);
+    cout << "Digite el año de lanzamiento del video juego: " << endl;
+    cin >> anio_lanzamiento;
+    aux.setAnioLanzamiento(anio_lanzamiento);
+    cout << "Digite la plataforma en la que se almacena el video juego: " << endl;
+    cout << "1- steam \n 2- Ea play \n 3- Ubisoft \n 4- Epic games \n 5- Gforece one" << endl;
+    cin >> plataforma;
+    switch (plataforma)
+    {
+    case 1:
+        aux.setPlataforma("Steam");
+        break;
+    case 2:
+        aux.setPlataforma("Ea play");
+        break;
+    case 3:
+        aux.setPlataforma("Ubisoft");
+        break;
+    case 4:
+        aux.setPlataforma("Epic games");
+        break;
+    case 5:
+        aux.setPlataforma("Gforece one");
+        break;
+
+    default:
+        cout << "opcion no valida" << endl;
+        break;
+    }
+
+    lista_juegos.push_back(aux);
+}
+
+void jugar_ahorcado(ahorcado bcs)
+{
+    string nombreJugador;
+    int eleccion2;
+    while (true)
+    {
+        int partidas = bcs.getPartidasGanadas();
+        clock_t tiempo = bcs.getTiempoJugado();
+        string aux;
+        cout << "Top Ganador: " << bcs.getNombreJugador() << endl;
+        cout << "Partidas ganadas: " << partidas << endl;
+        cout << "Tiempo total jugado: " << static_cast<double>(tiempo) / CLOCKS_PER_SEC << " segundos" << endl;
+        cout << "-------------------------------------------------------" << endl;
+        cout << "1- Jugar juego" << endl;
+        cout << "2- Salir" << endl;
+        cin >> eleccion2;
+
+        switch (eleccion2)
+        {
+        case 1:
+        {
+            cout << "Ingrese su nombre: ";
+            cin >> nombreJugador;
+            bcs.setNombreJugador(nombreJugador);
+            bcs.jugar_ahorcado(partidas, tiempo);
+            bcs.setPartidasGanadas(partidas);
+            bcs.setTiempoJugado(tiempo);
+            break;
+        }
+        case 2:
+            break;
+        default:
+            break;
+        }
+        if (eleccion2 == 2)
+            break;
+    }
+}
--- vista/mattel_game_service.cpp
+++ vista/mattel_game_service.cpp
@@ -12,15 +12,16 @@ int main()
     bcs.setPlataforma("Steam");
     bcs.setPartidasGanadas(0);
     bcs.setTiempoJugado(0);
-    string nombreJugador;
+
     int eleccion;
 
     while (true)
     {
         cout << "------------------------------- Mattel Game Service --------------------------------------" << endl;
         cout << " -- Bienvenido al sistema de juegos y clasificacion de mattel, que deseas hacer hoy ? ----" << endl;
-        cout << "-- 1- Jugar Ahorcado                                      --------------------------------" << endl;
-        cout << "-- 2- Salir                                               --------------------------------" << endl;
+        cout << "-- 1- Lista de juegos                                      -------------------------------" << endl;
+        cout << "-- 2- Jugar ahorcado                                               -----------------------" << endl;
+        cout << "-- 2- Salir                                                        -----------------------" << endl;
         cout << "------------------------------------------------------------------------------------------" << endl;
 
         cin >> eleccion;
@@ -28,43 +29,12 @@ int main()
         switch (eleccion)
         {
         case 1:
-            int eleccion2;
-            while (true)
-            {
-                int partidas = bcs.getPartidasGanadas();
-                clock_t tiempo = bcs.getTiempoJugado();
-                string aux;
-                cout << "Top Ganador: " << bcs.getNombreJugador() << endl;
-                cout << "Partidas ganadas: " << partidas << endl;
-                cout << "Tiempo total jugado: " << static_cast<double>(tiempo) / CLOCKS_PER_SEC << " segundos" << endl;
-                cout << "-------------------------------------------------------" << endl;
-                cout << "1- Jugar al ahorcado" << endl;
-                cout << "2- Salir" << endl;
-                cin >> eleccion2;
-
-                switch (eleccion2)
-                {
-                case 1:
-                {
-                    cout << "Ingrese su nombre: ";
-                    cin >> nombreJugador;
-                    bcs.setNombreJugador(nombreJugador);
-                    bcs.jugar_ahorcado(partidas, tiempo);
-                    bcs.setPartidasGanadas(partidas);
-                    bcs.setTiempoJugado(tiempo);
-                    break;
-                }
-                case 2:
-                    break; // Sal del bucle del juego sin salir del bucle principal
-                default:
-                    break;
-                }
-                if (eleccion2 == 2) // Si el jugador eligió salir desde el juego
-                    break;
-            }
 
             break;
         case 2:
+            jugar_ahorcado(bcs);
+            break;
+        case 3:
             return 0;
             break;
         default:
