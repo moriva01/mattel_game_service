4277cd40010a81bee75180fdcc958a18fa4cba11
DeibyOspina <159458425+DeibyOspina@users.noreply.github.com>
2024-04-05 13:39:37 -0500

    Update mattel_game_service.cpp

--- vista/mattel_game_service.cpp
+++ vista/mattel_game_service.cpp
@@ -1,1 +1,12 @@
 // clase main donde se ejecutara finalmente el aplicativo
+
+cuatro_en_raya ej;
+   
+    ej.setNombre("Cuatro En Raya");
+    ej.setCantJugadores(2);
+    ej.setCategoria("Estrategia")
+    ej.setDesarrollador("N/a");
+    ej.setAnioLanzamiento(1974);
+    ej.setPlataforma("Steam");
+    ej.setPartidasGanadas(0);
+    ej.setTiempoJugado(0);
